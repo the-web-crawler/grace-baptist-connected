2add1675fda5d98f5fbafe2cec02a74404b00959
the-web-crawler <109045184+the-web-crawler@users.noreply.github.com>
2024-06-08 17:26:48 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import {initializeApp} from 'firebase-admin/app';
 import {getAuth} from "firebase-admin/auth";
 
 const app = initializeApp({
-    credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
+    credential: admin.credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)),
     databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
 });
 

16fb80a6c7686420581da495995cf9ed7c20fbba
theWebCrawler <109045184+the-web-crawler@users.noreply.github.com>
2024-06-09 16:48:27 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,5 +1,4 @@
-console.log("App ran")
-console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+console.log("Notifying tokens");
 
 import admin from "firebase-admin";
 import {initializeApp} from 'firebase-admin/app';
@@ -15,9 +14,21 @@ const messaging = admin.messaging();
 const db = admin.database();
 
 const subUsers = Object.keys((await db.ref("subscribed").once("value")).val());
-console.log(subUsers)
+console.log(subUsers);
 
-subUsers.forEach(user => {
+const counter = {
+    n: 0,
+    count: function (n = -1) {
+        this.n += n;
+        if (this.n === 0) {
+            process.exit(0);
+        }
+    }
+};
+
+subUsers.forEach(async user => {
+    counter.count(1);
+
     const message = {
         token: user,
         notification: {
@@ -28,9 +39,15 @@ subUsers.forEach(user => {
 
     messaging.send(message)
         .then((response) => {
+            counter.count();
             console.log("\x1b[32m%s\x1b[0m", "Notification successfully sent to", user);
         })
         .catch((error) => {
+            counter.count();
             console.warn("Notification failed to send to", user);
         });
 });
+
+if (subUsers.length === 0) {
+    counter.count(0);
+}
